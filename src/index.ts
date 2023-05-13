52940fd8cbb7576ebafb521f675b969caee5ae0e
satoshi-sh <satoss1108@gmail.com>
2023-05-13 18:59:39 -0500

    Implemented search actor and director

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,36 @@ const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=
 
 const sql = postgres(URL, { ssl: "require" });
 
-app.get("/actors/:name", async (req: Request, res: Response) => {
+app.get("/actor/:name", async (req: Request, res: Response) => {
   const name: string = req.params.name;
-  console.log(name);
   try {
     const reviews =
-      await sql`SELECT movies.title, ratings.rating, ratings.votes  
+      await sql`SELECT movies.title, ratings.rating, ratings.votes, movies.year  
     FROM people 
     JOIN stars ON people.id = stars.person_id 
     JOIN movies ON stars.movie_id = movies.id 
     JOIN ratings ON movies.id = ratings.movie_id 
-    WHERE people.name ILIKE ${name};`;
+    WHERE people.name ILIKE ${name}
+    ORDER BY movies.year ASC, movies.title ASC;`;
+    res.json(reviews);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/director/:name", async (req: Request, res: Response) => {
+  const name: string = req.params.name;
+  console.log(name);
+  try {
+    const reviews =
+      await sql`SELECT movies.title, ratings.rating, ratings.votes, movies.year  
+    FROM people 
+    JOIN directors ON people.id = directors.person_id 
+    JOIN movies ON directors.movie_id = movies.id 
+    JOIN ratings ON movies.id = ratings.movie_id 
+    WHERE people.name ILIKE ${name}
+    ORDER BY movies.year ASC, movies.title ASC;`;
     console.log(reviews);
     res.json(reviews);
   } catch (error) {
