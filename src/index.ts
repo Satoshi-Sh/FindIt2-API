dcb4155dd2f9d559995d5374bc498f5d16593499
satoshi-sh <satoss1108@gmail.com>
2023-05-17 12:25:13 -0500

    Changed code for deploying

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ require("dotenv").config();
 
 const app = express();
 const port = 3050;
-
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, ORIGIN } =
+  process.env;
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://example.com"],
+  origin: ORIGIN ? ORIGIN : "http://localhost:3000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
 const sql = postgres(URL, { ssl: "require" });
@@ -22,7 +22,7 @@ app.get("/actor/:name", async (req: Request, res: Response) => {
   const name: string = req.params.name;
   try {
     const reviews =
-      await sql`SELECT movies.title, ratings.rating, ratings.votes, movies.year  
+      await sql`SELECT movies.id,movies.title, ratings.rating, ratings.votes, movies.year  
     FROM people 
     JOIN stars ON people.id = stars.person_id 
     JOIN movies ON stars.movie_id = movies.id 
@@ -57,7 +57,7 @@ app.get("/director/:name", async (req: Request, res: Response) => {
   const name: string = req.params.name;
   try {
     const reviews =
-      await sql`SELECT movies.title, ratings.rating, ratings.votes, movies.year  
+      await sql`SELECT movies.id,movies.title, ratings.rating, ratings.votes, movies.year  
     FROM people 
     JOIN directors ON people.id = directors.person_id 
     JOIN movies ON directors.movie_id = movies.id 
@@ -93,7 +93,7 @@ app.get("/search/movie/:name", async (req: Request, res: Response) => {
   const name: string = req.params.name;
   try {
     const data =
-      await sql`SELECT DISTINCT movies.title,movies.year,ratings.rating,ratings.votes
+      await sql`SELECT DISTINCT movies.id,movies.title,movies.year,ratings.rating,ratings.votes
     FROM movies  
     JOIN ratings ON movies.id = ratings.movie_id
     WHERE movies.title ILIKE '%' || ${name} || '%'
@@ -107,5 +107,7 @@ app.get("/search/movie/:name", async (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  if (!ORIGIN) {
+    console.log(`Server listening at http://localhost:${port}`);
+  }
 });
